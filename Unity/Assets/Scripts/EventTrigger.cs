8bfeb1f3a0bb2dbba014039b349a2ca026b7416e
Rami Ahmed Bock <RamiAhmedBock@gmail.com>
2015-02-07 10:12:59 +0100

    Introduced really simple placeholder level design

---
+++ Unity/Assets/Scripts/EventTrigger.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class EventTrigger : MonoBehaviour {
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Egg"))
+        {
+            StartCollision(other);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Egg"))
+        {
+            EndCollision(other);
+        }
+    }
+
+    protected abstract void StartCollision(Collider other);
+
+    protected abstract void EndCollision(Collider other);
+
+}
