7fb54f5b62488e39637eba1a2a031038f8c99c97
Rami Ahmed Bock <RamiAhmedBock@gmail.com>
2015-02-07 02:15:24 +0100

    Trying to get gamepad controllers to work

--- Unity/Assets/Scripts/PlayerController.cs
+++ Unity/Assets/Scripts/PlayerController.cs
@@ -3,13 +3,27 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
+    public float playerSpeed = 0.3f; 
+    public int playerNumber = 1;
+
 	// Use this for initialization
 	void Start () {
-	
+        Debug.Log("Player " + playerNumber + " ready!");
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+    public void Move(float deltaX, float deltaY)
+    {
+        this.transform.position = transform.position + new Vector3(deltaX, deltaY, 0f) * playerSpeed;
+        //Debug.Log("Player " + playerNumber + " moving with x: " + deltaX + ", y: " + deltaY);
+    }
+
+    public void Attack()
+    {
+        Debug.Log("Player " + playerNumber + " Attack!");
+    }
 }
