525ac505b07e18bb97ee1abbaadce66756f04ef9
Rami Ahmed Bock <RamiAhmedBock@gmail.com>
2015-02-07 02:45:20 +0100

    Reverted to just 1 player with standard Unity GamePad controls

--- Unity/Assets/Scripts/InputReceiver.cs
+++ Unity/Assets/Scripts/InputReceiver.cs
@@ -1,19 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
-using XInputDotNetPure;
 
 public class InputReceiver : MonoBehaviour {
 
     private PlayerController _playerController;
-    private GamePadState _padState;
-    private GamePadState _prevState;
-    private PlayerIndex _playerIndex;
 
 	// Use this for initialization
 	void Start () 
     {
+
+
         _playerController = this.GetComponent<PlayerController>();
-        _playerIndex = (PlayerIndex)_playerController.playerNumber;
 	}
 	
 	// Update is called once per frame
@@ -24,31 +21,17 @@ public class InputReceiver : MonoBehaviour {
             return;
         }
 
-        
-        GamePadState testState = GamePad.GetState(_playerIndex);
-        if (testState.IsConnected)
-        {
-            Debug.Log("Loaded GamePad " + _playerController.playerNumber);
-            _prevState = testState;
-            _padState = GamePad.GetState(_playerIndex);
-        }
-
-        //float horizontalMovement = Input.GetAxis("Horizontal " + _playerController.playerNumber);
-        //float verticalMovement = Input.GetAxis("Vertical " + _playerController.playerNumber);
-        float horizontalMovement = _padState.ThumbSticks.Left.X;
-        float verticalMovement = _padState.ThumbSticks.Left.Y;
-
+        float horizontalMovement = Input.GetAxis("Horizontal " + _playerController.playerNumber);
+        float verticalMovement = Input.GetAxis("Vertical " + _playerController.playerNumber);
         if (Mathf.Abs(horizontalMovement) > 0.2f || Mathf.Abs(verticalMovement) > 0.2f)
         {
             _playerController.Move(horizontalMovement, verticalMovement);
         }
 
-        //bool punched = Input.GetButton("Fire1 " + _playerController.playerNumber);
-        bool attacked = _prevState.Buttons.A == ButtonState.Released && _padState.Buttons.A == ButtonState.Pressed;
+        bool attacked = Input.GetButton("Punch " + _playerController.playerNumber);
         if (attacked)
         {
             _playerController.Attack();
-            GamePad.SetVibration(_playerIndex, _padState.Triggers.Left, _padState.Triggers.Right);
         }
 	}
 }
