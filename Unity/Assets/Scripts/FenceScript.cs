f08d6f0170865295489281808dee7f15c76bfc52
janborg <janborg@gmail.com>
2015-02-07 17:50:35 +0100

    Cam changed to perspective cam - MUCH BETTER! Fences can now be tipped Minor tweaks

---
+++ Unity/Assets/Scripts/FenceScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FenceScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter(Collision collision) {
+		if (collision.transform.tag == "Egg")
+		{
+			collision.gameObject.GetComponent<EggController>().Explode();
+			rigidbody.isKinematic = false;
+			Destroy(gameObject, 1f);
+		}
+	}
+
+}
