8d373d42579ab3ece21892ece85524572efcdb74
Rami Ahmed Bock <RamiAhmedBock@gmail.com>
2015-02-07 03:09:32 +0100

    Implemented a simple velocity-based movement

--- Unity/Assets/Scripts/PlayerController.cs
+++ Unity/Assets/Scripts/PlayerController.cs
@@ -3,8 +3,12 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
-    public float playerSpeed = 0.3f; 
+    public float maxPlayerSpeed = 20f;
+    public float playerSpeed = 10f; 
     public int playerNumber = 1;
+    public float dragFactor = 2f;
+
+    private Vector3 _velocity;
 
 	// Use this for initialization
 	void Start () {
@@ -12,18 +16,32 @@ public class PlayerController : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+    {
 	
 	}
 
+    private void FixedUpdate()
+    {
+        if (_velocity.sqrMagnitude > 0f)
+        {
+            Vector3 speed = _velocity * playerSpeed * Time.fixedDeltaTime;
+            this.transform.position = transform.position + speed;
+            _velocity -= speed * dragFactor;
+        }
+    }
+
     public void Move(float deltaX, float deltaY)
     {
-        this.transform.position = transform.position + new Vector3(deltaX, deltaY, 0f) * playerSpeed;
+        _velocity += new Vector3(deltaX, deltaY, 0f);
+        _velocity = Vector3.ClampMagnitude(_velocity, maxPlayerSpeed);
+        //this.transform.position = transform.position + new Vector3(deltaX, deltaY, 0f) * playerSpeed;
         //Debug.Log("Player " + playerNumber + " moving with x: " + deltaX + ", y: " + deltaY);
     }
 
     public void Attack()
     {
+        // TODO: Try to figure out how to vibrate the controller ?
         Debug.Log("Player " + playerNumber + " Attack!");
     }
 }
